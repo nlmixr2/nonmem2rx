d33b06c2e4d9f0ca99429951953d2169c2ec5a10
Matthew Fidler <matthew.fidler@gmail.com>
2023-01-25 23:50:34 -0600

    Add theta handling

---
+++ R/theta.R
@@ -0,0 +1,9 @@
+#' @export
+#' @rdname nonmem2rxRec
+nonmem2rxRec.the <- function(x) {
+  .x <- x
+  class(.x) <- NULL
+  for (i in .x) {
+    
+  }
+}
---
+++ src/theta.c
@@ -0,0 +1,24 @@
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdint.h>   /* dj: import intptr_t */
+//#include "ode.h"
+#include <rxode2parseSbuf.h>
+#include <errno.h>
+#include <dparser2.h>
+#include <R.h>
+#include <Rinternals.h>
+#include <R_ext/Rdynload.h>
+#include <Rmath.h>
+#ifdef ENABLE_NLS
+#include <libintl.h>
+#define _(String) dgettext ("nonmem2rx", String)
+/* replace pkg as appropriate */
+#else
+#define _(String) (String)
+#endif
+#include "theta.g.d_parser.h"
+
+extern D_ParserTables parser_tables_nonmem2rxTheta;
+
