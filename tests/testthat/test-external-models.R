868f9b3262c62a75a66e0dc45d8e99ad29e30628
Matthew Fidler <matthew.fidler@gmail.com>
2023-02-23 14:20:05 -0600

    Clean up messages/warnings

--- tests/testthat/test-external-models.R
+++ tests/testthat/test-external-models.R
@@ -1,55 +1,55 @@
+.nonmem2rx <- function(...) {
+  suppressWarnings(suppressMessages(nonmem2rx(...)))
+}
 test_that("test nonmemica models", {
   skip_if_not(requireNamespace("nonmemica", quietly = TRUE))
   
-  m1 <- nonmem2rx(system.file("project/model/1001/1001.ctl",package="nonmemica"))
+  m1 <- .nonmem2rx(system.file("project/model/1001/1001.ctl",package="nonmemica"))
   expect_length(m1$meta$validation, 4L)
   
-  m2 <- nonmem2rx(system.file("project/model/2001/2001.ctl",package="nonmemica"))
+  m2 <- .nonmem2rx(system.file("project/model/2001/2001.ctl",package="nonmemica"))
   expect_length(m2$meta$validation, 4L)
   
-  m3<- nonmem2rx(system.file("project/model/2002/2002.ctl",package="nonmemica"))
+  m3<- .nonmem2rx(system.file("project/model/2002/2002.ctl",package="nonmemica"))
   expect_length(m3$meta$validation, 4L)
    
-  m4 <- nonmem2rx(system.file("project/model/2003/2003.ctl",package="nonmemica"))
+  m4 <- .nonmem2rx(system.file("project/model/2003/2003.ctl",package="nonmemica"))
   expect_length(m4$meta$validation, 4L)
 
-  m5 <- nonmem2rx(system.file("project/model/2004/2004.ctl",package="nonmemica"))
+  m5 <- .nonmem2rx(system.file("project/model/2004/2004.ctl",package="nonmemica"))
   expect_length(m5$meta$validation, 4L)
 
-  m6 <- nonmem2rx(system.file("project/model/2005/2005.ctl",package="nonmemica"))
+  m6 <- .nonmem2rx(system.file("project/model/2005/2005.ctl",package="nonmemica"))
   expect_length(m6$meta$validation, 4L)
 
-  m7 <- nonmem2rx(system.file("project/model/2006/2006.ctl",package="nonmemica"))
+  m7 <- .nonmem2rx(system.file("project/model/2006/2006.ctl",package="nonmemica"))
   expect_length(m7$meta$validation, 4L)
   
-  m8 <- nonmem2rx(system.file("project/model/2007/2007.ctl",package="nonmemica"))
+  m8 <- .nonmem2rx(system.file("project/model/2007/2007.ctl",package="nonmemica"))
   expect_length(m8$meta$validation, 4L)
   
 })
 
 test_that("test nonemem2R models", {
-  
   skip_if_not(requireNamespace("nonmem2R", quietly = TRUE))
-  
-  m1 <- nonmem2rx(system.file("extdata/run001.mod",package="nonmem2R"))
-  
+  expect_error(.nonmem2rx(system.file("extdata/run001.mod",package="nonmem2R")), NA)
 })
 
 test_that("test NMdata models", {
   skip_if_not(requireNamespace("NMdata", quietly = TRUE))
-  m <- nonmem2rx(system.file("examples/nonmem/xgxr001.mod", package="NMdata"))
+  m <- .nonmem2rx(system.file("examples/nonmem/xgxr001.mod", package="NMdata"))
   expect_length(m$meta$validation, 2L)
   
-  m <- nonmem2rx(system.file("examples/nonmem/xgxr002.mod", package="NMdata"))
+  m <- .nonmem2rx(system.file("examples/nonmem/xgxr002.mod", package="NMdata"))
   expect_length(m$meta$validation, 2L)
 
-  m <- nonmem2rx(system.file("examples/nonmem/xgxr003.mod", package="NMdata"))
+  m <- .nonmem2rx(system.file("examples/nonmem/xgxr003.mod", package="NMdata"))
   expect_length(m$meta$validation, 2L)
 
-  m <- nonmem2rx(system.file("examples/nonmem/xgxr014.mod", package="NMdata"))
+  m <- .nonmem2rx(system.file("examples/nonmem/xgxr014.mod", package="NMdata"))
   expect_length(m$meta$validation, 2L)
 
-  m <- nonmem2rx(system.file("examples/nonmem/xgxr018.mod", package="NMdata"))
+  m <- .nonmem2rx(system.file("examples/nonmem/xgxr018.mod", package="NMdata"))
   expect_length(m$meta$validation, 2L)
   
 })
--- tests/testthat/test-full-parsing.R
+++ tests/testthat/test-full-parsing.R
@@ -1,3 +1,6 @@
+.nonmem2rx <- function(...) {
+  suppressMessages(nonmem2rx(...))
+}
 test_that("full parsing", {
-  expect_warning(nonmem2rx(system.file("run001.mod", package="nonmem2rx")))
+  expect_warning(.nonmem2rx(system.file("run001.mod", package="nonmem2rx")))
 })
