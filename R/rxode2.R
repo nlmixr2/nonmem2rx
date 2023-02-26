d2fb34921a3040a92072c2c7d8813fcd122fa6af
Matthew Fidler <matthew.fidler@gmail.com>
2023-02-25 23:02:20 -0600

    Initial attempt at rename with thetaMat/sigma preserved

--- R/rxode2.R
+++ R/rxode2.R
@@ -17,14 +17,14 @@
   list(.env, .x)
 }
 
-.dressAndSaveObj <- function(x, env) {
+.dressAndSaveObj <- function(x, env, compress=TRUE) {
   .new <- rxode2::rxUiDecompress(x)
   for (.v in .nonmem2rxExtraSave) {
     if (exists(.v, envir=env)) {
       assign(.v, get(.v, envir=env), envir=.new)
     }
   }
-  .new <- rxode2::rxUiCompress(.new)
+  if (compress) .new <- rxode2::rxUiCompress(.new)
   class(.new) <- c("nonmem2rx", class(.new))
   .new
 }
@@ -58,5 +58,42 @@ model.nonmem2rx <- function(x, ..., append = FALSE,
   rxode2::.modelHandleModelLines(.modelLines, .ret, modifyIni = FALSE, 
                                  append = append, auto = auto, envir = envir)
   .dressAndSaveObj(.ret, .tmp[[1]])
+}
+
+
+.rxnmRename1 <- function(rxui, lst) {
+  .thetaMat <- rxui$thetaMat
+  .dnt <- dimnames(.thetaMat)[[1]]
+  .sigma <- rxui$sigma
+  .dns <- dimnames(.sigma)[[1]]
+  .w <- which(.dnt == lst[[4]])
+  if (length(.w) == 1) {
+    .dnt[.w] <- lst[[3]]
+    dimnames(.thetaMat) <- list(.dnt, .dnt)
+    rxui$thetaMat <- .thetaMat
+  }
+  .w <- which(.dns == lst[[4]])
+  if (length(.w) == 1) {
+    .dns[.w] <- lst[[3]]
+    dimnames(.sigma) <- list(.dns, .dns)
+    rxui$sigma <- .sigma
+  }
+}
+
+
+#'@export
+rxRename.nonmem2rx <- function(.data, ...) {
+  .tmp <- .stripAndSaveObj(.data)
+  .ret <- .tmp[[2]]
+  .callInfo <- rxode2::.quoteCallInfoLines(match.call(expand.dots = TRUE)[-(1:2)])
+  .lst <- as.list(match.call()[-1])
+  .lst$.data <- .ret
+  .ret <- .dressAndSaveObj(do.call(rxode2::.rxRename, c(.lst, list(envir = parent.frame(2)))),
+                           .tmp[[1]], compress=FALSE)
+  ## now use call information to rename any other variables in `thetaMat` and `sigma`
+  lapply(seq_along(.lst), function(i) {
+    .rxnmRename1(.ret, .lst[[i]])
+  })
+  rxode2::rxUiCompress(.ret)
 }
 
