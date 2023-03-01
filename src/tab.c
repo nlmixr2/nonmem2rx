21704097a01a07b16cd124b9a0e3c3f0df99b0de
Matthew Fidler <matthew.fidler@gmail.com>
2023-02-28 18:37:11 -0600

    expand $table parsing errors

--- src/tab.c
+++ src/tab.c
@@ -29,9 +29,13 @@
 #define freeP nonmem2rx_tab_freeP
 #define parseFreeLast nonmem2rx_tab_parseFreeLast
 #define parseFree nonmem2rx_tab_parseFree
-
+#include "parseSyntaxErrors.h"
 
 extern D_ParserTables parser_tables_nonmem2rxTab;
+
+extern char *eBuf;
+extern int eBufLast;
+extern sbuf sbTransErr;
 
 char *gBuf;
 int gBufFree=0;
@@ -65,7 +69,7 @@ void parseFree(int last) {
 typedef void (print_node_fn_t)(int depth, char *token_name, char *token_value, void *client_data);
 
 void wprint_node_tab(int depth, char *name, char *value, void *client_data)  {
-  
+
 }
 
 extern char * rc_dup_str(const char *s, const char *e);
@@ -141,20 +145,24 @@ void trans_tab(const char* parse){
   curP->save_parse_tree = 1;
   curP->error_recovery = 1;
   curP->initial_scope = NULL;
-  //curP->syntax_error_fn = rxSyntaxError;
+    curP->syntax_error_fn = nonmem2rxSyntaxError;
   if (gBufFree) R_Free(gBuf);
   // Should be able to use gBuf directly, but I believe it cause
   // problems with R's garbage collection, so duplicate the string.
   gBuf = (char*)(parse);
+
   gBufFree=0;
+  eBuf = gBuf;
+  eBufLast = 0;
+  errP = curP;
+
   _pn= dparse(curP, gBuf, (int)strlen(gBuf));
   if (!_pn || curP->syntax_errors) {
     //rx_syntax_error = 1;
-    parseFree(0);
-    Rf_errorcall(R_NilValue, "parsing error for $TABLE");
   } else {
     wprint_parsetree_tab(parser_tables_nonmem2rxTab, _pn, 0, wprint_node_tab, NULL);
   }
+  finalizeSyntaxError();
 }
 
 SEXP nonmem2rxPushTableInfo(const char *file, int hasPred, int fullData,
