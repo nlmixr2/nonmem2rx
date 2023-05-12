096bec0d3e5aee2b060284ce95798f3c31c3b074
Matthew L. Fidler <matthew.fidler@gmail.com>
2023-05-11 22:42:25 -0500

    test/fix multiple endpoint iwres model

--- R/rxUiGet.R
+++ R/rxUiGet.R
@@ -8,8 +8,23 @@ rxUiGet.simulationModelIwres <- function(x, ...) {
     .env$.ui <- .ui
     .ui <- with(.env,eval(rxode2::rxCombineErrorLines(.ui, modelVars=TRUE)))
     DV <- sim <- iwres <- rxdv <- rx_pred_ <- rx_r_ <- NULL
-    .ret <- suppressMessages(rxode2::model(.ui, iwres <- (DV-rx_pred_)/sqrt(rx_r_),
-                                           append=sim, auto=FALSE))
+    if (length(.ui$predDf$cond) == 1) {
+      .ret <- suppressMessages(rxode2::model(.ui, iwres <- (DV-rx_pred_)/sqrt(rx_r_),
+                                             append=sim, auto=FALSE))
+    } else {
+      .ret <- suppressMessages(rxode2::as.rxUi(.ui))
+      .lstExpr <- .ret$lstExpr
+      .l <- length(.lstExpr)
+      while(identical(.lstExpr[[.l]][[1]], quote(`dvid`)) ||
+              identical(.lstExpr[[.l]][[1]], quote(`cmt`))) .l <- .l - 1
+      .lstOut <- c(list(quote(`{`)),
+                   lapply(seq_len(.l), function(i) .lstExpr[[i]]),
+                    list(quote(iwres <- (DV-rx_pred_)/sqrt(rx_r_))),
+                   lapply(seq(.l+1, length(.lstExpr)), function(i) .lstExpr[[i]]))
+      .lstOut <- as.call(list(quote(`model`), as.call(.lstOut)))
+      rxode2::model(.ret) <- .lstOut
+      .ret
+    }
     .ret <- rxode2::rxModelVars(.ret)
     .ret <- rxode2(.ret)
     .ret
