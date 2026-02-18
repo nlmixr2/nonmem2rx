1e80058dc4fd2c584169ff414b4516860f1c407e
mattfidler <matthew.fidler@gmail.com>
2026-02-17 20:47:40 -0600

    Add failing test

--- tests/testthat/test-lst-import.R
+++ tests/testthat/test-lst-import.R
@@ -15,3 +15,18 @@ test_that("lst import that didn't import cov", {
   expect_false(is.null(f$thetaMat))
 
 })
+
+test_that("lst import with errors with 2+ EPSs", {
+
+  if (!file.exists(test_path("test-lst-import-2.rds"))) {
+    skip("File 'test-lst-import-2.rds' does not exist.")
+  }
+
+  lst <- readRDS(test_path("test-lst-import-2.rds"))
+
+  # Save file in temporary directory
+  expect_error(withr::with_tempdir({
+    writeLines(lst, "run70.lst")
+    suppressMessages(nonmem2rx("run70.lst"))
+  }), NA)
+})
