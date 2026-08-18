43c61290fe8aec52656fb96080ff60100f01850c
mattfidler <matthew.fidler@gmail.com>
2026-08-17 23:11:22 -0500

    Use |> instead of %>% in test-piping.R to satisfy CodeFactor
    CodeFactor flagged 7 magrittr-pipe usages in this file as newly
    introduced style issues on PR #247.

--- tests/testthat/test-piping.R
+++ tests/testthat/test-piping.R
@@ -13,27 +13,27 @@ withr::with_options(list(nonmem2rx.save=FALSE, nonmem2rx.load=FALSE, nonmem2rx.o
     expect_false(is.null(f$nonmemData))
     expect_true(!is.null(f$dfSub))
 
-    f2 <- f %>% ini(eta1 ~ 0.1)
+    f2 <- f |> ini(eta1 ~ 0.1)
     expect_true(inherits(f2, "nonmem2rx"))
     expect_false(is.null(f2$nonmemData))
     expect_true(is.null(f2$dfSub))
 
-    f2 <- f %>% model(cl <- exp(theta1))
+    f2 <- f |> model(cl <- exp(theta1))
     expect_true(inherits(f2, "nonmem2rx"))
     expect_false(is.null(f2$nonmemData))
     expect_true(is.null(f2$dfSub))
 
-    f2 <- f %>% ini(eta1=fixed)
+    f2 <- f |> ini(eta1=fixed)
     expect_false(is.null(f2$nonmemData))
     expect_true(!is.null(f2$dfSub))
     expect_true(inherits(f2, "nonmem2rx"))
 
-    f2 <- f %>% rxRename(eta.v=eta2)
+    f2 <- f |> rxRename(eta.v=eta2)
     expect_false(is.null(f2$nonmemData))
     expect_true(!is.null(f2$dfSub))
     expect_true(inherits(f2, "nonmem2rx"))
 
-    f2 <- f %>% dplyr::rename(eta.v=eta2)
+    f2 <- f |> dplyr::rename(eta.v=eta2)
     expect_false(is.null(f2$nonmemData))
     expect_true(!is.null(f2$dfSub))
     expect_true(inherits(f2, "nonmem2rx"))
@@ -55,12 +55,12 @@ withr::with_options(list(nonmem2rx.save=FALSE, nonmem2rx.load=FALSE, nonmem2rx.o
     # (nlmixr2/rxode2#1149).
     expect_false(is.null(f$simulationModel))
 
-    fAuc <- f %>% model(d/dt(AUC) <- f, append=TRUE)
+    fAuc <- f |> model(d/dt(AUC) <- f, append=TRUE)
 
     expect_true("AUC" %in% rxode2::rxState(fAuc))
     expect_true("AUC" %in% rxode2::rxModelVars(fAuc$simulationModel)$state)
 
-    ev <- rxode2::et(amt=120000, ii=12, until=24) %>%
+    ev <- rxode2::et(amt=120000, ii=12, until=24) |>
       rxode2::et(c(0, 4, 8, 11.999, 12, 24))
 
     s <- rxode2::rxSolve(fAuc, ev, returnType="data.frame")
